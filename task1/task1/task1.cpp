904a902b1dbca63b2575ef8e9c0acd770739615c
Egovanros <119813882+Egovanros@users.noreply.github.com>
2023-06-26 15:03:15 +0300

    Add files via upload

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,30 @@
+﻿
+// задачи на перебор и циклы(задача 8)
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    ofstream file;
+    file.open("fractions.txt");
+    for (int denominator = 2; denominator <= 255; denominator++) {
+        for (int numerator = 1; numerator < denominator; numerator++) {
+            if (gcd(numerator, denominator) == 1) {
+                file << (double)numerator / denominator << endl;
+            }
+        }
+    }
+    file.close();
+    return 0;
+}
